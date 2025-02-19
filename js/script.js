ae73fee17d1a50a8175f20757922c5f1f0accc63
MDJAmin <mohammadamindf@gmail.com>
2025-02-19 19:23:12 +0330

    CC's

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ const resultSection = document.querySelector("#result");
 const borderSection = document.querySelector("#borders");
 btnEl.addEventListener("click", async () => {
 
-    resultSection.innerHTML=''
-    borderSection.innerHTML=''
-    resultSection.textContent="loading..."
+  resultSection.innerHTML = ''
+  borderSection.innerHTML = ''
+  resultSection.textContent = "loading..."
   try {
     const res = await fetch(
       `https://restcountries.com/v3.1/name/${inpEl.value}?fullText=true`
@@ -19,12 +19,12 @@ btnEl.addEventListener("click", async () => {
         <h2>${data[0].name.official}</h2>
         <p>${data[0].capital[0]}</p>
     </div>`;
-    data[0].borders.map(async(e)=>{
-        const res = await fetch(
-            `https://restcountries.com/v3.1/alpha/${e}`
-          );
-          const data = await res.json();
-          borderSection.innerHTML+=`<div class="card">
+    data[0].borders.map(async (e) => {
+      const res = await fetch(
+        `https://restcountries.com/v3.1/alpha/${e}`
+      );
+      const data = await res.json();
+      borderSection.innerHTML += `<div class="card">
           <img src="${data[0].flags.png}" alt="">
           <h2>${data[0].name.official}</h2>
           <p>${data[0].capital[0]}</p>
@@ -33,6 +33,6 @@ btnEl.addEventListener("click", async () => {
   } catch (err) {
     alert(err);
   }
-  inpEl.value=''
+  inpEl.value = ''
 
 });
